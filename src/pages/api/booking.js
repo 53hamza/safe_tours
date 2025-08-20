9e488aa3eb266e296c8f69478926f3c611f4d2d0
mufaddal <codewithmufaddal@gmail.com>
2025-08-21 01:18:53 +0530

    test again

--- src/pages/api/booking.js
+++ src/pages/api/booking.js
@@ -53,6 +53,7 @@ export default async function handler(req, res) {
     });
   } catch (error) {
     console.error("Error saving booking:", error);
+    
     return res.status(500).json({
       success: false,
       message: "Database error",
